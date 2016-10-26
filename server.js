887834b7cef3cdd48cc8e752cf23e9014e38cd48
George Ramirez <georgeramirez@Georges-MacBook-Pro.local>
2016-10-25 19:27:35 -0500

    two servers

--- server.js
+++ server.js
@@ -2,22 +2,36 @@ var http = require('http');
 
 //Lets define a port we want to listen to
 var PORT=7000;
-var PORT=7500;
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+    response.end('It Works!! Path Hit: ' + request.url+'\nTesting response on 7000');
+
 }
 
 //Create a server
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(7000, function(){
+server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log('Something good about yourself');
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
 
-server.listen(7500, function(){
+// ******************************
+var PORT2=7500;
+//We need a function which handles requests and send response
+function handleRequest2(request, response){
+    response.end('This should be PORT2 at 7500: ' + request.url+'\nTesting response on 7500');
+    
+
+}
+var server = http.createServer(handleRequest2);
+
+server.listen(PORT2, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log('Something bad about yourself');
+    console.log("Server listening on: http://localhost:%s", PORT2);
+
+
 });
